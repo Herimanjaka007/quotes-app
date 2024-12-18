4e82705612c97af2967e970129fa84c6e3c51d3d
myhrindra194 <myhrindra194@gmail.com>
2024-12-18 10:41:25 +0300

    Set view

--- lib/controllers/quote_controller.dart
+++ lib/controllers/quote_controller.dart
@@ -1,13 +1,14 @@
 import 'dart:math'; // Importez cette bibliothèque pour générer des nombres aléatoires
+
 import '../models/quote_model.dart';
 
 class QuoteController {
-  QuoteModel _model = QuoteModel();
+  final QuoteModel _model = QuoteModel();
 
   List<Quote> get quotes => _model.quotes;
 
   Quote randomQuotes() {
-    final _random = new Random();
-    return quotes[_random.nextInt(quotes.length)];
+    final random = Random();
+    return _model.getQuote(random.nextInt(quotes.length));
   }
 }
