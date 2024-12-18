984abef35e5e94214e7181fec198ed65c7620815
lovasoa-rm <lovasoa.ramahaly@gmail.com>
2024-12-18 11:05:30 +0100

    viewModel update

--- lib/viewModel/quote_viewModel.dart
+++ lib/viewModel/quote_viewModel.dart
@@ -1,12 +1,15 @@
-import 'dart:math'; // Importez cette bibliothèque pour générer des nombres aléatoires
+import 'dart:math';
+import 'package:flutter/material.dart';
+
 import '../models/quote_model.dart';
 
-class QuoteController {
+class QuoteController extends ChangeNotifier {
   QuoteModel _model = QuoteModel();
 
   List<Quote> get quotes => _model.quotes;
 
   Quote randomQuotes() {
+    notifyListeners();
     final _random = new Random();
     return quotes[_random.nextInt(quotes.length)];
   }
